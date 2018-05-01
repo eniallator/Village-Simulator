f50714cfdf36237cbe1b4c5c9d96a3b2f109fd12
eniallator <eniallatormc@gmail.com>
2018-05-01 17:55:47 +0100

    Got image loading working

--- src/Villager.js
+++ src/Villager.js
@@ -1,6 +1,10 @@
 import Entity from './Entity'
 import Task from './Task'
-import still from '../assets/images/villager/still.png'
+import still_path from '../assets/images/villager/still.png'
+
+const still = new Image()
+still.src = still_path
+
 
 const ACTIONS = {
     walk: function(task) { if (!this.hitbox.detectCollision(task.hitbox)) this.moveTo(task.hitbox.pos) }
@@ -8,10 +12,9 @@ const ACTIONS = {
 
 class Villager extends Entity {
     constructor(x, y) {
-        // console.log(typeof still, still)
-        // const sideLength = still.width * 2
+        const sideLength = still.width * 2
 
-        super(x, y, 16, 32)
+        super(x, y, sideLength, sideLength * 2)
 
         this.taskQueue = []
         this.speed = 1
@@ -45,11 +48,11 @@ class Villager extends Entity {
     }
 
     draw(ctx) {
-        // const img = still
-        // const imgWidth = img.width * 2
-        // const imgHeight = img.height * 2
+        const img = still
+        const imgWidth = img.width * 2
+        const imgHeight = img.height * 2
 
-        // ctx.drawImage(img, this.pos.x - imgWidth / 2, this.pos.y - imgHeight, imgWidth, imgHeight)
+        ctx.drawImage(img, this.pos.x - imgWidth / 2, this.pos.y - imgHeight, imgWidth, imgHeight)
     }
 }
 
