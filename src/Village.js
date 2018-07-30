0e243ee9660e8b316438495c77f0db0c912e470a
eniallator <eniallatormc@gmail.com>
2018-07-30 19:01:35 +0100

    Fixed villagers going nuts when villagecenter was added to the obstacles

--- src/Village.js
+++ src/Village.js
@@ -3,9 +3,11 @@ import Villager from './Villager'
 
 class Village {
     constructor(map, x, y) {
-        this.villageCentre = new VillageCentre(map, x, y)
+        this.villageCentre = new VillageCentre(x, y)
         this.map = map
         this.villagers = []
+
+        this.map.registerObstacle(this.villageCentre)
 
         for (var i = 0; i < 5; i++) {
             let newVillager = new Villager(this.map, Math.random() * 960, Math.random() * 540)
--- src/VillageCentre.js
+++ src/VillageCentre.js
@@ -1,8 +1,8 @@
 import Obstacle from './Obstacle'
 
 class VillageCentre extends Obstacle {
-    constructor(map, x, y) {
-        super(map, x, y, 40, 40)
+    constructor(x, y) {
+        super(x, y, 40, 40)
     }
 }
 
